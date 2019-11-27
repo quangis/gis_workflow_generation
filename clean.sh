0c695b92d126fc2f65c664a7d7f247ed4076c3e5
HanKruiger <hankruiger@gmail.com>
2019-11-27 11:36:07 +0100

    no impl folders and doc

--- clean.sh
+++ clean.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
+# Remove all APE output.
+
 for q in $(seq 1 5); do
     for o in sota full; do
         dir="./q${q}_${o}"
         rm -f ${dir}/figs/*
-        rm -f ${dir}/impls/*
         rm -f ${dir}/sat_solutions.txt
     done
 done
