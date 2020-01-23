0d1c6919d2498954e230305db22484572b37beff
HanKruiger <hankruiger@gmail.com>
2020-01-23 13:51:17 +0100

    Add unconstrained specifications, and revert to APE 0.8

--- run.sh
+++ run.sh
@@ -2,11 +2,15 @@
 
 # Run APE for all questions, for both ontologies.
 
-ape="APE-0.9.1.jar"
+ape="APE-0.8.jar"
 
 for q in $(seq 1 5); do
     for o in sota full; do
         config="./q${q}_${o}/ape.configuration"
         java -jar $ape $config
+        if [ -f "./q${q}_${o}_constrained/ape.configuration" ]; then
+            config="./q${q}_${o}_constrained/ape.configuration"
+            java -jar $ape $config
+        fi
     done
 done
